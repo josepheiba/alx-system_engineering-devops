daaffcb7cc81b1be723f893fcdce9f154a7419ef
Joseph Eiba <josepheiba@icloud.com>
2024-04-19 22:03:21 +0100

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,5 @@
-# 2-execute_a_command.pp
-
-exec { 'killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
-  path        => ['/usr/bin'],
-  onlyif      => '/usr/bin/pgrep -f killmenow',
-  refreshonly => true,
-}
-
-notify { 'Process killed successfully':
-  subscribe => Exec['killmenow'],
+# Executes a bash command
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
