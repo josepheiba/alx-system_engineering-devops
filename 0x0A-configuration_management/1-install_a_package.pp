0622883b8c79792ba88cd8a39ab9df19306a39c8
Joseph Eiba <josepheiba@icloud.com>
2024-04-19 21:53:20 +0100

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# 1-install_a_package.pp
-
-package { 'flask':
+# Install flask 2.1.0 using pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Exec['uninstall_flask'],  # Ensure the uninstallation is done before attempting installation
-}
-
-exec { 'uninstall_flask':
-  command => 'pip3 uninstall -y Flask==2.1.0',
-  path    => '/usr/local/bin',  # Adjust the path if necessary
-  onlyif  => 'pip3 show Flask | grep -q "Version: 2.1.0"',  # Check if Flask 2.1.0 is installed
+  provider => 'pip3'
 }
