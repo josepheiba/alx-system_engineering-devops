6eccd7e0b2549e940956c7555543074d7c3c1e2f
Joseph Eiba <josepheiba@icloud.com>
2024-04-19 22:04:23 +0100

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# 1-install_a_package.pp
-
-package { 'Flask':
+# Using Puppet, install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-package { 'python3-pip':
-  ensure => installed,
 }
