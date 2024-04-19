b54d76c91bfbbee6b7823d56a25f108ce19fdc78
Joseph Eiba <josepheiba@icloud.com>
2024-04-19 21:55:34 +0100

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,13 @@
-# kill_process.pp
+# 2-execute_a_command.pp
 
+# Define an exec resource to kill the process named "killmenow"
 exec { 'killmenow':
-  command     => 'pkill killmenow',
+  command     => '/usr/bin/pkill -f killmenow',
   path        => '/usr/bin',
   refreshonly => true,
+}
+
+# Notify when the command has been executed
+notify { 'Process killed successfully':
+  subscribe => Exec['killmenow'],
 }
