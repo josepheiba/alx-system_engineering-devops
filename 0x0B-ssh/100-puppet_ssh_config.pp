0f75616ed39918924580d113f0080bdc921ff8a4
Joseph Eiba <josepheiba@icloud.com>
2024-04-22 00:01:08 +0100

    ok

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,15 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
   ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
+file_line { 'Delare identity file':
   ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
