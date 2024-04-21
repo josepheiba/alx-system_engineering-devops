404f891ce078d311b45555868911e4ac2912d9e0
Joseph Eiba <josepheiba@icloud.com>
2024-04-21 23:54:15 +0100

    ok

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,6 +4,7 @@ include stdlib
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/sshd_config',
   line  => 'PasswordAuthentication no',
+  ensure => present,
 }
 
 file_line { 'Declare identity file':
