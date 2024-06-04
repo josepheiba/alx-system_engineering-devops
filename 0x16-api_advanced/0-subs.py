1241e87e4305b75738753492e8ec5ff8f30f0e91
Joseph Eiba <josepheiba@icloud.com>
2024-06-04 17:52:09 +0100

    bruh

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Function ro retrieve the number of subscribers for a specific subreddit."""
+"""
+file of number of subscrbers method
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        # "Users-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-        "User-Agent": "linux:0x16.api.advanced"
-
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """
+    number of subscribers method
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
