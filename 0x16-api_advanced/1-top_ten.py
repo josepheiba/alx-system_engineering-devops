10fb7736f2eb28159fddc49408dbd78df7b1d35f
Joseph Eiba <josepheiba@icloud.com>
2024-06-04 18:04:35 +0100

    bruh

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+file of number of subscribers method
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    number of subscribers method
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
