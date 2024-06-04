df866f7ad6772ff4b1333b9e7cd27ca8b33ec68d
Joseph Eiba <josepheiba@icloud.com>
2024-06-04 17:56:26 +0100

    bruh

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    number of subscribers method
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
