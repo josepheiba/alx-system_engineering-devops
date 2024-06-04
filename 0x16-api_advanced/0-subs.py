e69ba006d437fda2006c1ee2749a58bb06f0369e
Joseph Eiba <josepheiba@icloud.com>
2024-06-04 17:48:14 +0100

    bruh

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,8 +2,6 @@
 """
 0-subs.py
 """
-from requests import get
-from sys import argv
 import requests
 
 
@@ -11,16 +9,11 @@ def number_of_subscribers(subreddit):
     """
     0-subs.py
     """
-    head = {'User-Agent': 'Mozilla/5.0'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=head).json()
-    try:
-        return count.get('data').get('subscribers')
-    except requests.exceptions.HTTPError:
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-    except requests.exceptions.RequestException:
-        return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
