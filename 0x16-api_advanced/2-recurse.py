504e1fc86cfbbf25b3af91f8f9082986ec3e0ea1
Joseph Eiba <josepheiba@icloud.com>
2024-06-04 18:06:34 +0100

    bruh

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+file of number of subscribers method
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """
+    number of subscribers method
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "ok"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
