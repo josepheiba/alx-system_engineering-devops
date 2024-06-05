aee4112b131acf3195c0063e97623ae8982bf3a9
Joseph Eiba <josepheiba@icloud.com>
2024-06-05 18:11:13 +0100

    bruh

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""
-file of number of subscribers method
-"""
+"""Script that returns top 10 hot posts of a subreddit"""
 import requests
 
+after = None
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """
-    number of subscribers method
-    """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+
+def recurse(subreddit, hot_list=[]):
+    """Recursive function that returns a list of top posts"""
+    global after
+
+    headers = {"User-Agent": "youBot/2.1"}
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"after": after}
+    response = requests.get(
+        URL, params=params, headers=headers, allow_redirects=False
+    )
+
+    if response.status_code == 200:
+        after_data = response.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+
+        titles = response.json().get("data").get("children")
+        for title in titles:
+            hot_list.append(title.get("data").get("title"))
+        return hot_list
+    else:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
